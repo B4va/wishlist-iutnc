173ffd40124319470a86b28a54391767bc65f627
Louis Friedrich <louisf.28@hotmail.fr>
2020-04-16 15:47:45 +0200

    complète structure initiale

---
+++ src/controllers/ItemController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace wishlist\models;
+require_once './vendor/autoload.php';
+use \wishlist\models\Item;
+
+class ItemController {
+
+    public function getItem() {
+
+    }
+
+    public function render() {
+        
+    }
+}
+
+
+?>
