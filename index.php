c5fdd14d81aca756fbbc0b419ca325619c858021
Loïc Steinmetz <loic.steinmetz2@gmail.com>
2020-04-16 16:38:11 +0200

    implemente le layout html

--- index.php
+++ index.php
@@ -1,15 +1,35 @@
 <?php
 
 require_once 'vendor/autoload.php';
-use \wishlist\conf\ConnectionFactory;
+use \wishlist\utils\ConnectionFactory;
 use \wishlist\models\Item;
 use \wishlist\models\Liste;
 use \wishlist\models\User;
 
+use wishlist\utils\HtmlLayout;
+
 $app = new \Slim\Slim();
 $db = ConnectionFactory::makeConnection();
 
+$app->get('/', function(){
+    test();
+});
 
+function test(){
+    HtmlLayout::header('Test');
+    echo ('
+        <div class="jumbotron">
+            <div class="container">
+                <h1 class="display-4">Salut le gang !</h1>
+                <button type="button" class="btn btn-primary" id="btn">Vas y clique !</button>
+            </div>
+        </div>
+        <script>
+            document.getElementById("btn").addEventListener("click", () => alert("Salut"));
+        </script>
+    ');
+    HtmlLayout::footer();
+}
 
 $app->run();
 
