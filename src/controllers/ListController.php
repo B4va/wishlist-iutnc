fb8c6737f6abcfcac4928797e0da0930b057fa39
Louis Friedrich <louisf.28@hotmail.fr>
2020-04-19 18:35:24 +0200

    implémente fonctionnalités 6,7,20,21

--- src/controllers/ListController.php
+++ src/controllers/ListController.php
@@ -12,9 +12,27 @@ class ListController {
     /**
      * Créé une vue affichant toutes les listes publiques
      */
-    public function displayLists() {
+    public function displayLists() : void {
         $ensemble = Liste::orderBy('expiration', 'DESC')->get();
-        $v = new View(LIST_VIEW, ['title' => 'displayLists' , 'list' => $ensemble]);
+        $v = new View(LIST_VIEW, ['title' => 'Lists' , 'list' => $ensemble]);
+        $v->render();
+    }
+
+    /**
+     * Créé une vue affichant le formulaire de création de liste
+     */
+    public function displayListCreator() : void {
+        $v = new View(NEW_VIEW, ['title' => 'ListCreator']);
+        $v->render();
+    }
+
+    /**
+     * Créé une vue affichant le formulaire d'édition de liste
+     */
+    public function displayListEditor() : void {
+        // récupérer 'no' de la liste à modifier
+        $list = Liste::getById($no);
+        $v = new View(EDIT_VIEW, ['title' => 'ListEditor' , 'list' => $list]);
         $v->render();
     }
 }
