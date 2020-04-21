324e1ed6afd15c41f27e1079c1a39e670860e8ce
Loïc Steinmetz <loic.steinmetz2@gmail.com>
2020-04-21 16:47:11 +0200

    modifie les vues et ajoute les routes à lindex [à tester et corriger]

--- index.php
+++ index.php
@@ -6,10 +6,61 @@ use \wishlist\models\Item;
 use \wishlist\models\Liste;
 use \wishlist\models\User;
 
-use wishlist\views\ListView;
+use wishlist\controllers\ListController;
 
 $app = new \Slim\Slim();
 $db = ConnectionFactory::makeConnection();
+
+
+/**
+ * Général
+ */
+$app->get('/', function(){
+    ListController::displayLists();
+})->name('home');
+
+/**
+ * Listes
+ */
+$app->get('/lists', function(){
+    $slim = \Slim\Slim::getInstance();
+    $token = $slim->request->get()['token'];
+    ListController::displayObject($token);
+})->name('/list/new');
+$app->get('/', function(){
+    ListController::displayCreator();
+})->name('newList');
+$app->post('/', function(){
+    $slim = \Slim\Slim::getInstance();
+    $attr = [
+        $slim->request->post()['titre'],
+        $slim->request->post()['description'],
+        //$slim->request->post()['visibility'],
+        $slim->request->post()['expiration']
+    ];
+    ListController::create($attr);
+})->name('createList');
+$app->get('list/edit', function(){
+    $slim = \Slim\Slim::getInstance();
+    $token = $slim->request->get()['token'];
+    ListController::displayEditor($token);
+})->name('editList');
+$app->put('/list/update', function(){
+    $attr = [
+        $slim->request->put()['titre'],
+        $slim->request->put()['description'],
+        //$slim->request->put()['visibility'],
+        $slim->request->put()['expiration']
+    ];
+    ListController::update($attr);
+})->name('udpateList');
+$app->get('/', function(){
+    $slim = \Slim\Slim::getInstance();
+    $token = $slim->request->get()['token'];
+    ListController::delete($token);
+})->name('deleteList');
+    
+// TESTS
 
 $app->get('/list', function(){
     testList();
--- src/views/ListView.php
+++ src/views/ListView.php
@@ -16,166 +16,50 @@ class ListView extends View {
      * @return string code html
      */
     protected function list() : string{
-        return <<<html
+        $slim = \Slim\Slim::getInstance();
+        $newList = $slim->urlFor('newList');
+        $html = <<<html
 
         <div class="jumbotron">
             <div class="container">
                 <h1 class='display-4'>Bienvenue sur Wishlist</h1>
                 <hr class="my-4">
-                <button type="button" class="btn btn-primary" id="btn">Créer une liste</button>
+                <a href='$newList' class="btn btn-primary" id="btn">Créer une liste</a>
             </div>
         </div>
+
+html;
+
+        foreach($var['lists'] as $list){
+            $showList = $slim->urlFor('showList', $list->token);
+            $editList = $slim->urlFor('editList', $list->token);
+            $html += <<<html
 
         <!-- Boucler sur les listes passées en paramètre -->
         <div class="col-lg-6 offset-lg-3 col-md-8 offset-md-2 col-sm-10 offset-sm-1 my-5">
             <h1 class="text-center text-primary mb-5">Les dernières listes</h1>
             <div class="card my-4">
-                <div class="card-header">
+                <div class="card-header bg-white">
                     <div class="row">
                         <div class="col-8">
                             <!-- Insérer variable nom -->
-                            <h3 class="m-0">Nom de la liste</h3>
-                            <!-- Insérer variable auteur -->
-                            <p class="text-small m-0">Auteur Delaliste</p>
+                            <h3 class="m-0"><a href='$showList'>$list->titre</a></h3>
                         </div>
                         <div class="col-4 d-flex justify-content-end align-items-center">
-                            <!-- Vérifier si liste de l'utilisateur conntecté -->
-                            <a href="#" class="btn btn-danger">Modifier</a>
+                            <!-- Vérifier si liste de l'utilisateur conntecté ? -->
+                            <a href="$editList" class="btn btn-danger">Modifier</a>
                         </div>
                     </div>
                 </div>
-                <div class="card-body">
-                    <!-- Insérer variable description -->
-                    <p class="card-text">Description de la liste - Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-                    <ul class="mt-3 mb-0">
-                        <!-- Boucler sur le items de la liste -->
-                        <li>Item</li>
-                        <li>Item</li>
-                        <li>Item</li>
-                        <li>Item</li>
-                        <li>Item</li>
-                        <li>Item</li>
-                    </ul>
-                </div>
                 <div class="card-footer">
                     <!-- Insérer variable expiration -->
-                    <p class="text-muted m-0">Expire le 03/04/2020</p>
-                </div>
-            </div>
-        </div>
-
-        <!-- Exemples à supprimer -->
-
-        <!-- Boucler sur les listes passées en paramètre -->
-        <div class="col-lg-6 offset-lg-3 col-md-8 offset-md-2 col-sm-10 offset-sm-1 my-5">
-            <div class="card my-4">
-                <div class="card-header">
-                    <div class="row">
-                        <div class="col-8">
-                            <!-- Insérer variable nom -->
-                            <h3 class="m-0">Nom de la liste</h3>
-                            <!-- Insérer variable auteur -->
-                            <p class="text-small m-0">Auteur Delaliste</p>
-                        </div>
-                        <div class="col-4 d-flex justify-content-end align-items-center">
-                            <!-- Vérifier si liste de l'utilisateur conntecté -->
-                            <a href="#" class="btn btn-danger">Modifier</a>
-                        </div>
-                    </div>
-                </div>
-                <div class="card-body">
-                    <!-- Insérer variable description -->
-                    <p class="card-text">Description de la liste - Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-                    <ul class="mt-3 mb-0">
-                        <!-- Boucler sur le items de la liste -->
-                        <li>Item</li>
-                        <li>Item</li>
-                        <li>Item</li>
-                        <li>Item</li>
-                        <li>Item</li>
-                        <li>Item</li>
-                    </ul>
-                </div>
-                <div class="card-footer">
-                    <!-- Insérer variable expiration -->
-                    <p class="text-muted m-0">Expire le 03/04/2020</p>
-                </div>
-            </div>
-        </div>
-        <!-- Boucler sur les listes passées en paramètre -->
-        <div class="col-lg-6 offset-lg-3 col-md-8 offset-md-2 col-sm-10 offset-sm-1 my-5">
-            <div class="card my-4">
-                <div class="card-header">
-                    <div class="row">
-                        <div class="col-8">
-                            <!-- Insérer variable nom -->
-                            <h3 class="m-0">Nom de la liste</h3>
-                            <!-- Insérer variable auteur -->
-                            <p class="text-small m-0">Auteur Delaliste</p>
-                        </div>
-                        <div class="col-4 d-flex justify-content-end align-items-center">
-                            <!-- Vérifier si liste de l'utilisateur conntecté -->
-                            <a href="#" class="btn btn-danger">Modifier</a>
-                        </div>
-                    </div>
-                </div>
-                <div class="card-body">
-                    <!-- Insérer variable description -->
-                    <p class="card-text">Description de la liste - Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-                    <ul class="mt-3 mb-0">
-                        <!-- Boucler sur le items de la liste -->
-                        <li>Item</li>
-                        <li>Item</li>
-                        <li>Item</li>
-                        <li>Item</li>
-                        <li>Item</li>
-                        <li>Item</li>
-                    </ul>
-                </div>
-                <div class="card-footer">
-                    <!-- Insérer variable expiration -->
-                    <p class="text-muted m-0">Expire le 03/04/2020</p>
-                </div>
-            </div>
-        </div>
-        <!-- Boucler sur les listes passées en paramètre -->
-        <div class="col-lg-6 offset-lg-3 col-md-8 offset-md-2 col-sm-10 offset-sm-1 my-5">
-            <div class="card my-4">
-                <div class="card-header">
-                    <div class="row">
-                        <div class="col-8">
-                            <!-- Insérer variable nom -->
-                            <h3 class="m-0">Nom de la liste</h3>
-                            <!-- Insérer variable auteur -->
-                            <p class="text-small m-0">Auteur Delaliste</p>
-                        </div>
-                        <div class="col-4 d-flex justify-content-end align-items-center">
-                            <!-- Vérifier si liste de l'utilisateur conntecté -->
-                            <a href="#" class="btn btn-danger">Modifier</a>
-                        </div>
-                    </div>
-                </div>
-                <div class="card-body">
-                    <!-- Insérer variable description -->
-                    <p class="card-text">Description de la liste - Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-                    <ul class="mt-3 mb-0">
-                        <!-- Boucler sur le items de la liste -->
-                        <li>Item</li>
-                        <li>Item</li>
-                        <li>Item</li>
-                        <li>Item</li>
-                        <li>Item</li>
-                        <li>Item</li>
-                    </ul>
-                </div>
-                <div class="card-footer">
-                    <!-- Insérer variable expiration -->
-                    <p class="text-muted m-0">Expire le 03/04/2020</p>
+                    <p class="text-muted m-0">Expire le $list->expiration</p>
                 </div>
             </div>
         </div>
 
 html;
+        }
 
     }
 
@@ -184,28 +68,34 @@ html;
      * @return string code html
      */
     protected function edit() : string {
+        $slim = \Slim\Slim::getInstance();
+        $list = $var['list'];
+        $updateList = $slim->urlFor('updateList');
+        //$deleteList = $slim->urlFor('deleteList', $list);
+        $home = $slim->urlFor('home');
         return <<<html
 
         <div class="col-lg-6 offset-lg-3 col-md-8 offset-md-2 col-sm-10 offset-sm-1 my-5">
             <h1 class="text-center text-primary">Modifier ma liste</h1>
             <div class="card my-4 p-4">
-                <form>
+                <form method='POST' action='$updateList'>
                     <div class="form-group mb-3">
                         <label for="list_name">Nom</label>
                         <!-- Insérer l'ancien nom de la liste -->
-                        <input type="text" class="form-control" id="list_name" value="Ancien nom de la liste de souhait">
+                        <input type="text" class="form-control" id="list_name" value="$list->titre">
                     </div>
                     <div class="form-group mb-3">
                         <label for="list_description">Description</label>
                         <!-- Insérer l'ancienne description de la liste -->
-                        <textarea class="form-control" id="list_description" rows="6" style="resize: none;">Ancienne description de la liste</textarea>
+                        <textarea class="form-control" id="list_description" rows="6" style="resize: none;">$list->description</textarea>
                     </div>
                     <div class="form-group mb-4">
                         <label for="expiration_date">Date d'expiration</label>
                         <!-- Insérer l'ancienne date d'expiration de la liste -->
-                        <input type="date" class="form-control" id="expiration_date" value="2020-05-04">
+                        <input type="date" class="form-control" id="expiration_date" value="$list->expiration">
                     </div>
                     <div class="form-check mb-4">
+                        <!-- Sélectionner l'ancienne visibilité de la liste -->
                         <input class="form-check-input" type="checkbox" value="public" id="public_check">
                         <label class="form-check-label" for="public_check">
                             Liste publique
@@ -213,8 +103,8 @@ html;
                     </div>
                     <div class="form-group">
                         <button type="submit" class="btn btn-primary">Valider</button>
-                        <a href="#" class="btn btn-danger ml-3">Supprimer</a>
-                        <a href="#" class="btn btn-outline-secondary ml-3">Retour</a>
+                        <a href="<!--$deleteList-->#" class="btn btn-danger ml-3">Supprimer</a>
+                        <a href="$home" class="btn btn-outline-secondary ml-3">Retour</a>
                     </div>
                 </form>
             </div>
@@ -229,25 +119,25 @@ html;
      * @return string code html
      */
     protected function new() : string {
+        $slim = \Slim\Slim::getInstance();
+        $createList = $slim->urlFor('createList');
+        $home = $slim->urlFor('home');
         return <<<html
 
         <div class="col-lg-6 offset-lg-3 col-md-8 offset-md-2 col-sm-10 offset-sm-1 my-5">
             <h1 class="text-center text-primary">Créer une liste</h1>
             <div class="card my-4 p-4">
-                <form>
+                <form method='POST' action='$createList'>
                     <div class="form-group mb-3">
                         <label for="list_name">Nom</label>
-                        <!-- Insérer l'ancien nom de la liste -->
                         <input type="text" class="form-control" id="list_name" placeholder="Nom de ma liste">
                     </div>
                     <div class="form-group mb-3">
-                        <label for="list_description">Description</label>
                         <!-- Insérer l'ancienne description de la liste -->
                         <textarea class="form-control" id="list_description" rows="6" style="resize: none;" placeholder="Description de ma liste"></textarea>
                     </div>
                     <div class="form-group mb-4">
                         <label for="expiration_date">Date d'expiration</label>
-                        <!-- Insérer l'ancienne date d'expiration de la liste -->
                         <input type="date" class="form-control" id="expiration_date">
                     </div>
                     <div class="form-check mb-4">
@@ -258,7 +148,7 @@ html;
                     </div>
                     <div class="form-group">
                         <button type="submit" class="btn btn-primary">Valider</button>
-                        <a href="#" class="btn btn-outline-secondary ml-3">Retour</a>
+                        <a href="$home" class="btn btn-outline-secondary ml-3">Retour</a>
                     </div>
                 </form>
             </div>
