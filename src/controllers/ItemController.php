d2a19a62626f837d81c9ec8db78b021c58b52975
Louis Friedrich <louisf.28@hotmail.fr>
2020-04-29 14:27:50 +0200

    paramètre ItemController

--- src/controllers/ItemController.php
+++ src/controllers/ItemController.php
@@ -21,17 +21,17 @@ class ItemController implements ControllerOperations {
     /**
      * Créé une vue affichant le formulaire de création d'un item
      */
-    public function displayCreator() : void {
-        $v = new ItemView(CREATE_VIEW, ['title' => 'Nouvel item']);
+    public function displayCreator($idList) : void {
+        $v = new ItemView(CREATE_VIEW, ['title' => 'Nouvel item',  'idList' => $idList]);
         $v->render();
     }
 
     /**
      * Créé une vue affichant le formulaire d'édition d'un item
      */
-    public function displayEditor($token) : void {
-        $item = Item::getByToken($token);
-        $v = new ItemView(EDIT_VIEW, ['title' => 'Edition de l\'item' . $item->nom, 'object' => $item]);
+    public function displayEditor($id, $idList) : void {
+        $item = Item::getById($id);
+        $v = new ItemView(EDIT_VIEW, ['title' => 'Edition de l\'item' . $item->nom, 'object' => $item, 'idList' => $idList]);
         $v->render();
     }
 
@@ -39,20 +39,12 @@ class ItemController implements ControllerOperations {
      * Créé une vue affichant un item choisi par son id
      * @param [$id] identifiant de l'objet
      */
-    public function displayObject($id) : void {
-        $item = Item::getById($id);
-        $v = new ItemView(OBJECT_VIEW, ['title' => 'Item ' . $item->nom, 'object' => $item]);
-        $v->render();
-    }
+    public function displayObject($id) : void {}
 
     /**
      * Créé une vue affichant tous les items
      */
-    public function displayObjects() : void {
-        $ensemble = Item::getAll();
-        $v = new ItemView(OBJECTS_VIEW, ['title' => 'Items', 'objects' => $ensemble]);
-        $v->render();
-    }
+    public function displayObjects() : void {}
 
     /* 
              _________________________
