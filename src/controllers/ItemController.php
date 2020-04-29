121f2ec6d181f3177740e0d937a29bff9454cd36
Louis Friedrich <louisf.28@hotmail.fr>
2020-04-29 13:47:59 +0200

    rectif bis

--- src/controllers/ItemController.php
+++ src/controllers/ItemController.php
@@ -1,10 +1,14 @@
 <?php
 
 namespace wishlist\controllers;
-require_once './vendor/autoload.php';
-use \wishlist\models\Item;
 
-class ItemController {
+require_once './vendor/autoload.php';
+
+use \wishlist\controllers\ControllerOperations;
+use \wishlist\models\Item;
+use \wishlist\views\ItemView;
+
+class ItemController implements ControllerOperations {
 
 }
 
